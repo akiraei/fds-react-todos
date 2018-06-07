39df6b9b65837cabf7f91fee74e42a9acda8927b
akiraei <akiraeiphoto@gmail.com>
2018-06-07 15:38:51 +0900

    todos moved to state, css styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
 
 
-const todos = [
-  {id: 1
-  ,body: "react study"
-  ,complete: true
-  }
-  ,{
-    id:2
-    ,body: "redux study"
-    ,complete: false
-  }
-]
-
-
-
 class App extends Component {
+
+  state = {
+    todos: [
+    {id: 1
+    ,body: "react study"
+    ,complete: true
+    }
+    ,{
+      id:2
+      ,body: "redux study"
+      ,complete: false
+    }
+  ]
+  }
+
+
   render() {
+    const {todos} = this.state
     return (
-      <div container> 
+      <div> 
      
       <div>React</div>
 
       <h1>todo list</h1>
       <ul> 
         {
-          todos.map(todo => (
-            <li key={todo.id}>{todo.body}</li>
-            
+         todos.map(todo => (
+            <li className={ todo.complete ? 'complete' : ""} key={todo.id}>{todo.body}</li>
           ))
-
         }
       </ul>
+
+      <form>
+        <input/> >
+
+
+
+      </form>
            
+
+
       </div>
     );
   }

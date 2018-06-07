1ba5d3894a6c0987ff929c113eb19063124ffc71
akiraei <akiraeiphoto@gmail.com>
2018-06-07 15:58:06 +0900

    i don't know what is going on~~~ add function input and submit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,73 @@
 import React, { Component } from 'react';
 
 
+
+
 class App extends Component {
 
+  
   state = {
     todos: [
-    {id: 1
-    ,body: "react study"
-    ,complete: true
-    }
-    ,{
-      id:2
-      ,body: "redux study"
-      ,complete: false
-    }
-  ]
+      {id: count++
+        ,body: "react study"
+        ,complete: true
+      }
+      ,{
+        id: count++
+        ,body: "redux study"
+        ,complete: false
+      }
+    ],
+    newTodoBody: ""
+  }
+  
+
+  handleInputChange = e => {
+    this.setState({
+      newTodoBody: e.target.value
+    })
   }
 
+  handleButtonClick = e => {
+    e.preventDefault()
+    if(this.state.newTodoBody) {
+      const newTodo = {
+        body: this.state.newTodoBody,
+        complete: false,
+        id: count++
+      }
 
+      this.setState({
+        todos: [
+          ...this.state.todos, newTodo
+        ],
+        newTodoBody:''
+      })
+    }
+  }
+
+  
   render() {
-    const {todos} = this.state
+    const {todos, newTodoBody} = this.state
+    let count = 0;
     return (
       <div> 
      
       <div>React</div>
 
       <h1>todo list</h1>
+
+      <form>
+        <label>
+          input
+        <input type="text" value={newTodoBody} onChange={this.handleInputChange}/>
+        <button onClick={this.handleButtonClick}>submit</button>
+        </label>
+
+
+
+      </form>
+
       <ul> 
         {
          todos.map(todo => (
@@ -33,13 +75,6 @@ class App extends Component {
           ))
         }
       </ul>
-
-      <form>
-        <input/> >
-
-
-
-      </form>
            
 
 
